fc81dbd657081bcf74f2ff0a3a22b87e428a65db
Vineeth Venudasan <vineethv@thoughtworks.com>
2024-10-10 09:25:51 +0200

    working streaming implementation

--- src/main/kotlin/com/demo/largeexceldownload/LargeExcelDownloadApplication.kt
+++ src/main/kotlin/com/demo/largeexceldownload/LargeExcelDownloadApplication.kt
@@ -1,10 +1,13 @@
 package com.demo.largeexceldownload
 
+import jakarta.persistence.EntityManager
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody
 import java.io.OutputStream
@@ -19,7 +22,9 @@ fun main(args: Array<String>) {
 }
 
 @Controller
-class WebController {
+class WebController(
+    private val service: AppService
+) {
 
     @GetMapping("/export")
     fun export() : ResponseEntity<StreamingResponseBody> {
@@ -29,4 +34,32 @@ class WebController {
         }
         return ResponseEntity(stream, HttpStatus.OK)
     }
+
+    @GetMapping("/excelexport")
+    fun excelexport() : ResponseEntity<StreamingResponseBody> {
+        val stream = StreamingResponseBody { out: OutputStream ->
+            service.export(out)
+        }
+
+        return ResponseEntity(stream, HttpStatus.OK)
+    }
+}
+
+@Service
+class AppService(
+    private val repository: ProductRepository,
+    private val entityManager: EntityManager
+) {
+
+    @Transactional(readOnly = true)
+    fun export(out: OutputStream) {
+        repository.streamAll().use { body ->
+            body.forEach { item: Product ->
+                val msg = item.sku
+                out.write(msg.toByteArray())
+                entityManager.detach(item)
+            }
+        }
+    }
+
 }
--- src/main/kotlin/com/demo/largeexceldownload/ProductRepository.kt
+++ src/main/kotlin/com/demo/largeexceldownload/ProductRepository.kt
@@ -1,7 +1,22 @@
 package com.demo.largeexceldownload
 
+import jakarta.persistence.QueryHint
+import org.hibernate.jpa.QueryHints.*
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.jpa.repository.QueryHints
 import org.springframework.stereotype.Repository
+import java.util.stream.Stream
 
 @Repository
-interface ProductRepository : JpaRepository<Long, Product>
+interface ProductRepository : JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p")
+    @QueryHints(value = [
+        QueryHint(name = HINT_FETCH_SIZE, value = "10"),
+        QueryHint(name = HINT_CACHEABLE, value = "false"),
+        QueryHint(name = HINT_READONLY, value = "true"),
+    ])
+    fun streamAll(): Stream<Product>
+
+}
