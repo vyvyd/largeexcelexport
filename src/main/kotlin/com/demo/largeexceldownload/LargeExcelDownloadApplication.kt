67f32e4a65b558556712a5c75ad6856f43d7eb83
Vineeth Venudasan <vineethv@thoughtworks.com>
2024-10-10 08:23:27 +0200

    adding a streaming response controller

--- src/main/kotlin/com/demo/largeexceldownload/LargeExcelDownloadApplication.kt
+++ src/main/kotlin/com/demo/largeexceldownload/LargeExcelDownloadApplication.kt
@@ -2,10 +2,31 @@ package com.demo.largeexceldownload
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody
+import java.io.OutputStream
+import java.time.Instant
+
 
 @SpringBootApplication
 class LargeExcelDownloadApplication
 
 fun main(args: Array<String>) {
     runApplication<LargeExcelDownloadApplication>(*args)
+}
+
+@Controller
+class WebController {
+
+    @GetMapping("/export")
+    fun export() : ResponseEntity<StreamingResponseBody> {
+        val stream = StreamingResponseBody { out: OutputStream ->
+            val msg = "/srb" + " @ " + Instant.now().toString()
+            out.write(msg.toByteArray())
+        }
+        return ResponseEntity(stream, HttpStatus.OK)
+    }
 }
