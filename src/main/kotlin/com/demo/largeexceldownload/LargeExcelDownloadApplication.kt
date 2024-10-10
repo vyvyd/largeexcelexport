7e8455d67ac2bc3365e986b3f7d1520a3638d759
Vineeth Venudasan <vineethv@thoughtworks.com>
2024-10-10 08:18:53 +0200

    adding a db with a lot of rows

---
+++ src/main/kotlin/com/demo/largeexceldownload/LargeExcelDownloadApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.largeexceldownload
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LargeExcelDownloadApplication
+
+fun main(args: Array<String>) {
+    runApplication<LargeExcelDownloadApplication>(*args)
+}
---
+++ src/test/kotlin/com/demo/largeexceldownload/LargeExcelDownloadApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demo.largeexceldownload
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LargeExcelDownloadApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
